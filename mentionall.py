9f588f2a3048e7788f68480642718172bb7d60af
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-07-14 00:16:18 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -516,15 +516,12 @@ async def mentionall(tagadmin):
   
   if mode == "text_on_reply":
     anlik_calisan.append(event.chat_id)
- 
-    usrnum = 0
-    usrtxt = ""
-      
-         async for usr in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-       
-           usrnum += 1
-         
-          usrtxt += f"👥-[{usr.first_name}](tg://user?id={usr.id})\n "
+          usrnum = 0
+          usrtxt = ""  
+          async for usr in client.iter_participants(event.chat_id,filter=ChannelParticipantsAdmins):    
+        #  async for usr in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):     
+           usrnum += 1 
+           usrtxt += f"👥-[{usr.first_name}](tg://user?id={usr.id})\n "
         
          if event.chat_id not in anlik_calisan:
          
