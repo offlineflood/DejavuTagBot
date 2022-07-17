a476b567bf012dea78fd288be91449c9d136e601
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-07-18 01:44:50 +0400

    Update pyrogram_modul.py

--- pyrogram_modul.py
+++ pyrogram_modul.py
@@ -12,20 +12,20 @@ app = Client(
     bot_token=Config.BOT_TOKEN,
 )
 
-#@app.on_message(filters.command("start"))
-#async def _py(client: Client, message: Message):
- #   await message.reply_text('Pyrogram is a Python library for Telegram bots.')
-
-#@app.on_message(filters.new_chat_members, group=1)
-#async def hg(bot: Client, msg: Message):
- #   for new_user in msg.new_chat_members:
-  #      if str(new_user.id) == str(Config.BOT_ID):
-      #      await msg.reply(
-      #          f'''`**🤖Salam` {msg.from_user.mention} `\n🤖Məni` {msg.chat.title} `Qrupa əlavə etdiyiniz üçün təşəkkürlər⚡️`\n\n🤖Qurup'da User'ləri Tag Edmə Xususiyətinə Malik'əm.
+  @app.on_message(filters.command("start"))
+  async def _py(client: Client, message: Message):
+    await message.reply_text('Pyrogram is a Python library for Telegram bots.')
+ 
+  @app.on_message(filters.new_chat_members, group=1)
+  async def hg(bot: Client, msg: Message):
+     for new_user in msg.new_chat_members:
+         if str(new_user.id) == str(Config.BOT_ID):
+           await msg.reply(
+         f'''`**🤖Salam` {msg.from_user.mention} `\n🤖Məni` {msg.chat.title} `Qrupa əlavə etdiyiniz üçün təşəkkürlər⚡️`\n\n🤖Qurup'da User'ləri Tag Edmə Xususiyətinə Malik'əm.
 #Ətraflı Məlumat üçün /help-ə toxunun.**''')
 
-    #    elif str(new_user.id) == str(Config.OWNER_ID):
-          #  await msg.reply('[UstaTaggerBot](https://t.me/Ustataggerbot)-un Sahibi Qurupa Qatildı.\n\nXoş Gəldin Sahib😍.')
+        elif str(new_user.id) == str(Config.OWNER_ID):
+           await msg.reply('[UstaTaggerBot](https://t.me/Ustataggerbot)-un Sahibi Qurupa Qatildı.\n\nXoş Gəldin Sahib😍.')
 
  
     @app.on_message(filters.command("id"))
@@ -40,26 +40,26 @@ app = Client(
  
     await message.reply(out_str)
 
-#@app.on_message(filters.command("info"))
-#async def _id(_, message: Message):
-  #  msg = message.reply_to_message or message
- #   out_str = "**User İnfo:**\n"
-  #  out_str += f" ⚡️ __Grup ID__ : `{(msg.forward_from_chat or msg.chat).id}`\n"
-   # out_str += f" 💎 __Yanıtlanan Kullanıcı Adı__ : {msg.from_user.first_name}\n"
-    #out_str += f" 💬 __Mesaj ID__ : `{msg.forward_from_message_id or msg.message_id}`\n"
-    #if msg.from_user:
-     #   out_str += f" 🙋🏻‍♂️ __Yanıtlanan Kullanıcı ID__ : `{msg.from_user.id}`\n"
+  @app.on_message(filters.command("info"))
+  async def _id(_, message: Message):
+    msg = message.reply_to_message or message
+   out_str = "**User İnfo:**\n"
+    out_str += f" ⚡️ __Grup ID__ : `{(msg.forward_from_chat or msg.chat).id}`\n"
+    out_str += f" 💎 __Yanıtlanan Kullanıcı Adı__ : {msg.from_user.first_name}\n"
+    out_str += f" 💬 __Mesaj ID__ : `{msg.forward_from_message_id or msg.message_id}`\n"
+    if msg.from_user:
+        out_str += f" 🙋🏻‍♂️ __Yanıtlanan Kullanıcı ID__ : `{msg.from_user.id}`\n"
  
-    #await message.reply(out_str)
+    await message.reply(out_str)
 
-#@app.on_message(filters.command("ping"))
-#async def pingy(client, message):
-  #  start = datetime.now()
- #   hmm = await message.reply("Pong!")
-#    end = datetime.now()
-   # ms = (end - start).microseconds / 1000
-  #  await hmm.edit(
- #       f"█▀█ █▀█ █▄░█ █▀▀ █ \n█▀▀ █▄█ █░▀█ █▄█ ▄\n**Ping: {round(ms)}**")
+  @app.on_message(filters.command("ping"))
+  async def pingy(client, message):
+    start = datetime.now()
+    hmm = await message.reply("Pong!")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await hmm.edit(
+        f"█▀█ █▀█ █▄░█ █▀▀ █ \n█▀▀ █▄█ █░▀█ █▄█ ▄\n**Ping: {round(ms)}**")
     
 app.start()
 print(f"Bot pyrogram ( {pyrogram.__version__} sürümü ile başlatıldı. ")
