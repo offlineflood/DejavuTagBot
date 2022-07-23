2c1a25566d6fdb16a8afe35336a4eeeb508281f9
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-07-23 14:34:45 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
+anlik_calisan = []
+
+@client.on(events.NewMessage(pattern="^/admins ?(.*)"))
+async def tag_admin(event):
+    chat = await event.get_input_chat()
+    text = "♕︎Adminlər Siyahısı♕︎"
+    async for x in event.client.iter_participants(chat, 100, filter=ChannelParticipantsAdmins):
+        text += f"✰ \n [{x.first_name}](tg://user?id={x.id})"
+    if event.reply_to_msg_id:
+        await event.client.send_message(event.chat_id, text, reply_to=event.reply_to_msg_id)
+    else:
+        await event.reply(text)
+    raise StopPropagation
+
+print(">> Bot İşləyir 🚀 @ThrHassan Məlumat Ala bilərsiniz <<")
+client.run_until_disconnected()
