6b7bf628945c5da0c8e5b99f26081e61cf010d09
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-07-23 14:54:44 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,12 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = int(os.environ.get("APP_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
-client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+#api_id = int(os.environ.get("APP_ID"))
+#api_hash = os.environ.get("API_HASH")
+#bot_token = os.environ.get("BOT_TOKEN")
+#client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
-anlik_calisan = []
+#anlik_calisan = []
 
 @client.on(events.NewMessage(pattern="^/admins ?(.*)"))
 async def tag_admin(event):
