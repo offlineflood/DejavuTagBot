62a2d732582ce745c036d8b25497cc973917cfd6
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-07-23 15:09:14 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -442,7 +442,7 @@ async def tag_admin(event):
     chat = await event.get_input_chat()
     text = "♕︎Adminlər Siyahısı♕︎"
     async for x in event.client.iter_participants(chat, 100, filter=ChannelParticipantsAdmins):
-        text += f"✰ \n [{x.first_name}](tg://user?id={x.id})"
+        text += f" \n ➮ [{x.first_name}](tg://user?id={x.id})"
     if event.reply_to_msg_id:
         await event.client.send_message(event.chat_id, text, reply_to=event.reply_to_msg_id)
     else:
