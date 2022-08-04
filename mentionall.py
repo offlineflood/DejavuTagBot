7da52667e74f48d6a77795637786046e393b402a
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-08-04 15:26:27 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -30,20 +30,20 @@ anlik_calisan = []
 tekli_calisan = []
 
 
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (msg.content.toLowerCase().includes('of')) msg.reply('Of deme ah de!');
-  if (msg.content.toLowerCase().includes('ah')) msg.reply('Ah deme oh de!');
-  if (msg.content.toLowerCase().includes('oh')) msg.reply('Oh deme püf de!');
-  if (msg.content.toLowerCase().includes('püf')) msg.reply('Püf deme of de!');
-  if (msg.content.toLowerCase().includes('günaydın')) msg.reply('sana da günaydın');
-  if (msg.content.toLowerCase().includes('herkese günaydın')) msg.reply('günaydın :)');
-  if (msg.content.toLowerCase().includes('iyi geceler')) msg.reply('sana da iyi geceler');
-  if (msg.content.toLowerCase().includes('sa')) msg.reply('as');
-  if (msg.content.toLowerCase().includes('iyi akşamlar')) msg.reply('sana da iyi akşamlar');
-  if (msg.content.toLowerCase().includes('selamın aleyküm')) msg.reply('aleyküm selam');
-  if (msg.content.toLowerCase().includes('güle güle')) msg.reply('sana da güle güle');
-});
+#client.on('message', msg => {
+ # if (msg.author.bot) return;
+  #if (msg.content.toLowerCase().includes('of')) msg.reply('Of deme ah de!');
+  #if (msg.content.toLowerCase().includes('ah')) msg.reply('Ah deme oh de!');
+  #if (msg.content.toLowerCase().includes('oh')) msg.reply('Oh deme püf de!');
+  #if (msg.content.toLowerCase().includes('püf')) msg.reply('Püf deme of de!');
+  #if (msg.content.toLowerCase().includes('günaydın')) msg.reply('sana da günaydın');
+  #if (msg.content.toLowerCase().includes('herkese günaydın')) msg.reply('günaydın :)');
+  #if (msg.content.toLowerCase().includes('iyi geceler')) msg.reply('sana da iyi geceler');
+  #if (msg.content.toLowerCase().includes('sa')) msg.reply('as');
+  #if (msg.content.toLowerCase().includes('iyi akşamlar')) msg.reply('sana da iyi akşamlar');
+  #if (msg.content.toLowerCase().includes('selamın aleyküm')) msg.reply('aleyküm selam');
+  #if (msg.content.toLowerCase().includes('güle güle')) msg.reply('sana da güle güle');
+#});
 
 
 
@@ -569,14 +569,11 @@ async def tag_admin(event):
 #@client.on(events.NewMessage(pattern="^/admins ?(.*)"))
 
 
-#@client.on('ready', () => {
- # console.log(`Logged in as ${client.user.username}!`);
-#});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
 
-
-
-
-@client.on('message', msg => {
+client.on('message', msg => {
   if (msg.author.bot) return;
   if (msg.content.toLowerCase().includes('of')) msg.reply('Of deme ah de!');
   if (msg.content.toLowerCase().includes('ah')) msg.reply('Ah deme oh de!');
@@ -591,8 +588,7 @@ async def tag_admin(event):
   if (msg.content.toLowerCase().includes('güle güle')) msg.reply('sana da güle güle');
 });
 
-
-
+client.login(settings.token);
 
 
 
