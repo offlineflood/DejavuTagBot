a3a7169c67052b7be2251982b83799fb8a41c110
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-09-08 16:46:57 +0400

    Update pyrogram_modul.py

--- pyrogram_modul.py
+++ pyrogram_modul.py
@@ -18,15 +18,11 @@ async def hg(bot: Client, msg: Message):
     for new_user in msg.new_chat_members:
         if str(new_user.id) == str(Config.BOT_ID):
             await msg.reply(
-                f'''`Salam` {msg.from_user.mention} `Məni` {msg.chat.title} `Qrupa əlavə etdiyiniz üçün təşəkkürlər⚡️` \n\n **🤖Qruplardakı userləri tag Edmə üçün Yaradıldım.\n🤖Kömək üçün /help yazmaq kifayətdir.✨**''', 
+                f'''`Salam` {msg.from_user.mention} `Məni` {msg.chat.title} `Qrupa əlavə etdiyiniz üçün təşəkkürlər⚡️` \n\n **🤖Qruplardakı userləri tag Edmə üçün Yaradıldım.\n🤖Kömək üçün /start yazmaq kifayətdir.✨**''', 
             reply_markup=InlineKeyboardMarkup(
-      [
-        [
-          InlineKeyboardButton(
-            "Usta Bots💡", url=f"https://t.me/ustabots")
-        ],
-      ]
-     ))
+            [[InlineKeyboardButton("💡İnfo", url=f"https://t.me/UstaTagbot?start=start")],
+             [InlineKeyboardButton("𝐔𝐒𝐓𝐀 𝐁𝐎𝐓𝐋𝐀𝐑 👨‍💻", url=f"https://t.me/ustabots"),
+             InlineKeyboardButton("Qrup💬", url=f"https://t.me/Bizim_Paytaxt")]]))
 
         elif str(new_user.id) == str(Config.OWNER_ID):
             await msg.reply('🤖 [𓆩𓄂𝙰𝚂𝚀🇦🇿 𝑇𝑎𝑔𝑔𝑒𝑟 𝘉𝘰𝘵](https://t.me/UstaTagbot)-un Sahibi, Qrupa Qatıldı.\n Xoş Gəldin  Aramıza Sahib, Necəsən?🥰.')
