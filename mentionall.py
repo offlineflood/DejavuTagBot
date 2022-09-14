7900b82d3a2e23b103e42fbe18fe7b4d100ca8b5
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-09-14 15:28:21 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -29,7 +29,6 @@ bot_name = Config.BOT_NAME
 
 
 SUDO_USERS = Config.SUDO_USERS
-OWNER_ID = Config.OWNER_ID
 
 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
@@ -39,30 +38,6 @@ anlik_calisan = []
 tekli_calisan = []
   
 #--------------------------	
-def get_arg(message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
-@client.on(events.NewMessage(pattern="^/broadcast$"))
-#@client.on(events.NewMessage(from_users=OWNER_ID, pattern="^/broadcast$"))
-async def broadcast(client, message):
-    to_send = get_arg(message)
-    chats = load_chats_list()
-    success = 0
-    failed = 0
-    for chat in chats:
-        try:
-            await brend.send_message(int(chat), to_send)
-            success += 1
-        except:
-            failed += 1
-            remove_chat_from_db(str(chat))
-            pass
-    await message.reply(f"Mesaj {success} gruba gönderildi. {failed} grub başarısız")
 
 
 #•━━━━━━━━•••━━━━━━━━•
