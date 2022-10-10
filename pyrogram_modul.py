bcade92b6242912625f912967d075c203b248fbb
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2022-10-10 12:44:54 +0400

    Update pyrogram_modul.py

--- pyrogram_modul.py
+++ pyrogram_modul.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters, idle
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton #düyməsi yoxdur.!
 import pyrogram
 from Config import Config
 from datetime import datetime
@@ -7,78 +7,27 @@ from telethon import Button
 
 
 app = Client(
-    "MentionAll",
+    "User Tag Bot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
 )
 
+#---------------------------------------------------------------GROUP GIREKEN SALAMLAMA MSJ------------------------------------------------------------------------------#
 @app.on_message(filters.new_chat_members, group=1)
 async def hg(bot: Client, msg: Message):
     for new_user in msg.new_chat_members:
         if str(new_user.id) == str(Config.BOT_ID):
             await msg.reply(
                 f'''`Salam` {msg.from_user.mention} `Məni` {msg.chat.title} `Qrupa əlavə etdiyiniz üçün təşəkkürlər⚡️` \n\n **🤖Qruplardakı userləri tag Edmə üçün Yaradıldım.\n🤖Kömək üçün /start yazmaq kifayətdir.✨**''')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
+
+#-------------------------------------------------------------OWNERS SALAMLAMA MSJ---------------------------------------------------------------------------------------#
         elif str(new_user.id) == str(Config.OWNER_ID):
             await msg.reply('🤖 [𓆩𓄂𝙰𝚂𝚀🇦🇿 𝑇𝑎𝑔𝑔𝑒𝑟 𝘉𝘰𝘵](https://t.me/UstaTagbot)-un Sahibi, Qrupa Qatıldı.\n Xoş Gəldin  Aramıza Sahib, Necəsən?🥰.')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-            buttons = [[InlineKeyboardButton("⚡️Qrupa Əlavə ed⚡️",url="http://t.me/UstaDC_bot?startgroup=a"),
-                    InlineKeyboardButton("💻 Sahib", url="https://t.me/UstaNakhid"),
-                    InlineKeyboardButton("💡Usta Bots", url="https://t.me/ustabots")]]
-
-
-
-#@app.on_message(filters.new_chat_members, group=1)
-#async def hg(bot: Client, msg: Message):
-   # for new_user in msg.new_chat_members:
-    #    if str(new_user.id) == str(Config.BOT_ID):
-    #        await msg.reply(
-      #          f'''`Salam` {msg.from_user.mention} `Məni` {msg.chat.title} `Qrupa əlavə etdiyiniz üçün təşəkkürlər⚡️` \n\n **🤖Qruplardakı Userləri Tag Edmə üçün Yaradıldım.\n🤖Kömək üçün /help yazmaq kifayətdir.**''')
-
-#elif str(new_user.id) == str(Config.OWNER_ID):
-#await msg.reply('🤖 [Usta Tagger Bot](https://t.me/Ustataggerbot)-un Sahibi, Qrupa Qatıldı.\n Xoş Gəldin  Aramıza Sahib, Necəsən?🥰.')
-
-#elif str (new_user.id) == str(Config.OWNER_ID):
-            #await msg.reply('İşte bu gelen benim sahibim.')
-
- 
-#@app.on_message(filters.command("id"))
-#async def _id(_, message: Message):
- #   msg = message.reply_to_message or message
-  #  out_str = "**User İnfo:**\n"
-  #  out_str += f" ⚡️ __Grup ID__ : `{(msg.forward_from_chat or msg.chat).id}`\n"
-  #  out_str += f" 💎 __Yanıtlanan Kullanıcı Adı__ : {msg.from_user.first_name}\n"
-   # out_str += f" 💬 __Mesaj ID__ : `{msg.forward_from_message_id or msg.message_id}`\n"
-  #  if msg.from_user:
-     #   out_str += f" 🙋🏻‍♂️ __Yanıtlanan Kullanıcı ID__ : `{msg.from_user.id}`\n"
- 
-   # await message.reply(out_str)
-
-#@app.on_message(filters.command("info"))
-#async def _id(_, message: Message):
- #   msg = message.reply_to_message or message
-  #  out_str = "**İsdifadəçi İd'si:**\n"
-   # out_str += f" ⚡️ __Qrup İd'si__ : `{(msg.forward_from_chat or msg.chat).id}`\n"
-   # out_str += f" 🙋🏻‍♂️ __Cavab verən İstifadəçi Adı__ : {msg.from_user.first_name}\n"
-   # out_str += f" 💬 __Mesaj İd'si__ : `{msg.forward_from_message_id or msg.message_id}`\n"
-   # if msg.from_user:
-    #    out_str += f" 🙋🏻‍♂️ __Cavab verən İstifadəçi İd'si__ : `{msg.from_user.id}`\n"
- 
-#    await message.reply(out_str)
-
-#@app.on_message(filters.command("ping"))
-#async def pingy(client, message):
- #   start = datetime.now()
-  #  hmm = await message.reply("USTA BOTS==>[USTA Bots](https://t.me/ustabots)\nSAHIB==>[USTA Bots <Sahibi>](https://t.me/UstaNakhid)")
-   # end = datetime.now()
-    #ms = (end - start).microseconds / 1000
-  #  await hmm.edit(
-   #     f"**◤✞Ping✞◥⇎ {round(ms)}**\n\nUSTA BOTS==>[USTA Bots](https://t.me/ustabots)\nSAHIB==>[USTA Bots <Sahibi>](https://t.me/UstaNakhid)")
-    
-#elif str(new_user.id) == str(Config.OWNER_ID):
-#await msg.reply('🤖 [Usta Tagger Bot](https://t.me/Ustataggerbot)-un Sahibi, Qrupa Qatıldı.\n Xoş Gəldin  Aramıza Sahib, Necəsən?🥰.')
- 
 app.start()
-print(f"Bot pyrogram ( {pyrogram.__version__} sürümü ile başlatıldı. ")
+print(f"Bot piroqramı ( {pyrogram.__version__} ilə başladı... Botu işə salın!")
 idle()
